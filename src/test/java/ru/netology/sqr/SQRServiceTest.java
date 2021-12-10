ed9fcc05f0a29473f5c5e05ee2dad60dc343d4f2
Sarmat <def2413@bk.ru>
2021-12-10 19:24:29 +0500

    Fixed Test v2

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,9 +9,9 @@ class SQRServiceTest {
     @Test
     void calculate() {
         SQRService sqrService = new SQRService();
-        int num = 300;
         int expected = 3;
-        int actual = sqrService.calculate(num);
+        int sum = 0;
+        int actual = sqrService.calculate(sum);
 
         assertEquals(expected, actual);
 
