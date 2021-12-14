c01c1f55c64b04c9fee85c96db6213ca6bc06220
Sarmat <def2413@bk.ru>
2021-12-14 19:49:57 +0500

    FixTest v3

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,12 +1,12 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate(int sum) // какие параметры указывать в методе? Я не понимаю.
-    {
-        sum = 0;
-            for (int i = 10; i < 100; i++) {
-            if (i * i >= 200 && i * i <= 300) {
-               sum = sum + 1;
+    public int calculate(int from, int before) {
+
+        int sum = 0;
+        for (int i = 10; i < 100; i++) {
+            if (i * i >= from && i * i <= before) {
+                sum = sum + 1;
             }
         }
         return sum;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,13 +7,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SQRServiceTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateTotalRange() {
         SQRService sqrService = new SQRService();
+        int from = 200;
+        int before = 300;
         int expected = 3;
-        int sum = 0;
-        int actual = sqrService.calculate(sum);
+        int actual = sqrService.calculate(from, before);
 
         assertEquals(expected, actual);
 
+    }
+
+
+    @Test
+    void shouldCalculateBoundariesFrom() {
+        SQRService sqrService = new SQRService();
+        int from = 100;
+        int before = 102;
+        int expected = 1;
+        int actual = sqrService.calculate(from, before);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateBoundariesBefore() {
+        SQRService sqrService = new SQRService();
+        int from = 9800;
+        int before = 9900;
+        int expected = 1;
+        int actual = sqrService.calculate(from, before);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMaxRange() {
+        SQRService sqrService = new SQRService();
+        int from = 0;
+        int before = 10000;
+        int expected = 90;
+        int actual = sqrService.calculate(from, before);
+
+        assertEquals(expected, actual);
     }
 }
